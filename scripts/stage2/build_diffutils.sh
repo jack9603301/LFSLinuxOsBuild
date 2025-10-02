9b6cc1b314030e44ca3dddd2d76e101b76d95a2e
Ouyang Chunhui <jack9603301@163.com>
2025-10-02 21:51:30 +0800

    fix Step 11: gl_cv_func_strcasecmp_works=y
    Signed-off-by: Ouyang Chunhui <jack9603301@163.com>

--- scripts/stage2/build_diffutils.sh
+++ scripts/stage2/build_diffutils.sh
@@ -8,6 +8,7 @@ tar xvf "./diffutils-3.12.tar.xz"
 cd diffutils-3.12
 ./configure --prefix=/usr   \
             --host=$LFS_TGT \
+            gl_cv_func_strcasecmp_works=y  \
             --build=$(./build-aux/config.guess)
 make
 make DESTDIR=$LFS install

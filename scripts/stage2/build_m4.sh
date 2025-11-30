752e13ef6fda7600bdc000424c3c5867e0a55a4d
Ouyang Chunhui <jack9603301@163.com>
2025-11-30 14:43:00 +0800

    update
    Signed-off-by: Ouyang Chunhui <jack9603301@163.com>

--- scripts/stage2/build_m4.sh
+++ scripts/stage2/build_m4.sh
@@ -5,7 +5,7 @@ echo "Step 7: Compile M4 1.4.20(From Stage2)"
 
 cd $LFS/sources
 tar xvf "./m4-1.4.20.tar.xz"
-cd m4-1.4.19
+cd m4-1.4.20
 ./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess)
